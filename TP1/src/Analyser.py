11935a391dd5ab4423538b6a279720e90ef9d074
Margarida Ferreira <up201905046@fe.up.pt>
2022-04-21 18:36:51 +0100

    Change in analysis.txt format

--- TP1/src/Analyser.py
+++ TP1/src/Analyser.py
@@ -1,4 +1,3 @@
-import re
 import UnequalLengthMazes as ULM
 from Algorithms import SearchProblemsAlgorithms, algorithmTypes
 import boards
@@ -7,40 +6,36 @@ class Analyser:
 
     def analyse(self, n_heuristics):
         f = open("analysis.txt", "w")
-        for n in range(len(boards.initBoards)):
-            f.write("BOARD %s" % n)
-            ULM.setInitState(n)
+        for board in range(len(boards.initBoards)):
+            ULM.setInitState(board)
             problem = SearchProblemsAlgorithms(ULM.initState, ULM.isFinalState, ULM.newTransitions)
             for algorithm in algorithmTypes:
-                f.write('\n' + algorithm + ": ")
 
-                if (algorithm == "depth_limited"):
-                    lowerLimit = self.calculateLowerLimit(boards.initBoards[n])
-                    f.write(self.analyseDepthLimit(problem, lowerLimit))
-                elif (algorithm == "greedy" or algorithm == "A*"):
-                    f.write(self.analyseHeuristics(problem, algorithm, n_heuristics))
-                else:
+                #if (algorithm == "depth_limited"):
+                    #lowerLimit = self.calculateLowerLimit(boards.initBoards[n])
+                    #f.write(self.analyseDepthLimit(problem, algorithm, lowerLimit))
+                if (algorithm == "greedy" or algorithm == "A*"):
+                    f.write(self.analyseHeuristics(board, problem, algorithm, n_heuristics))
+                elif(algorithm != "depth_limited" and algorithm != "iterative_deepening"):
                     problem.run(algorithm)
-                    f.write(self.getSolutionStatisticsStr(problem.getSolution()))
+                    f.write(self.getSolutionStatisticsStr(board, problem.getSolution(), algorithm, 0))
 
             f.write('\n')
         f.close()
     
-    def analyseDepthLimit(self, problem, lowerLimit):
+    def analyseDepthLimit(self, board, problem, algorithm, lowerLimit):
         resultStr = ""
-        # for limit in range(lowerLimit, lowerLimit+2):
         for limit in range(lowerLimit, round(lowerLimit*1.5)):
             resultStr += "\n limit " + str(limit) + ": "
             problem.run("depth_limited", limit=limit)
-            resultStr += self.getSolutionStatisticsStr(problem.getSolution())
+            resultStr += self.getSolutionStatisticsStr(board, problem.getSolution(), algorithm, 0)
         return resultStr
 
-    def analyseHeuristics(self, problem, algorithm, n_heuristics):
+    def analyseHeuristics(self, board, problem, algorithm, n_heuristics):
         resultStr = ""
-        for heuristic in range(n_heuristics):
-            resultStr += "\n heuristic " + str(heuristic) + ": "
+        for heuristic in range(1, n_heuristics+1):
             problem.run(algorithm, heuristic=heuristic)
-            resultStr += self.getSolutionStatisticsStr(problem.getSolution())
+            resultStr += self.getSolutionStatisticsStr(board, problem.getSolution(), algorithm, heuristic)
         return resultStr
 
     def calculateLowerLimit(self, initBoard):
@@ -49,9 +44,9 @@ class Analyser:
             limit += initBoard[line].count(0)
         return limit
 
-    def getSolutionStatisticsStr(self, solution):
+    def getSolutionStatisticsStr(self, board, solution, algorithm, heuristic):
             (path, depth, nodes, time) = solution
-            solutionStatisticsStr = str(time) + ", " + str(nodes) + ", " + str(depth)
+            solutionStatisticsStr = str(board) + "," + algorithm + "," + str(heuristic) + "," + str(time) + "," + str(nodes) + "," + str(depth) + '\n'
             return solutionStatisticsStr
 
 
