dd2c9b642f15451bf8ce037d0cb87e171d406926
Patricia Oliveira <up201905427@fe.up.pt>
2022-05-18 12:21:47 +0100

    Add scatter plots / correlation between evety two columns

--- TP2/src/preprocessing.py
+++ TP2/src/preprocessing.py
@@ -2,6 +2,7 @@ from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
+import numpy as np
 
 # ---------
 # LOAD DATA
@@ -17,13 +18,20 @@ print(data['class'].unique())
 # sb.pairplot(data.dropna(), hue='class')
 
 # FIRST DATA ANALYSIS
-# plt.figure(figsize=(10, 10))
 
-# for column_index, column in enumerate(data.columns):
-#     if column == 'class':
-#         continue
-#     plt.subplot(2, 2, column_index + 1)
-#     sb.violinplot(x='class', y=column, data=data)
+plt.rcParams.update({'figure.figsize':(10,8), 'figure.dpi':100})
+
+columns = data.columns[:-1]
+pairs = [(columns[i], columns[j]) for i in range(len(columns) - 1) for j in range(i+1, len(columns))]
+for column1, column2 in pairs:
+    x = data[column1]
+    y = data[column2]
+
+    plt.scatter(x, y, label= f'y2 Correlation = {np.round(np.corrcoef(x,y)[0,1], 2)}')
+    plt.title(column1 + ' vs ' + column2)
+    plt.legend()
+    plt.show()
+
 
 # -------------
 # DATA CLEANING
@@ -31,9 +39,9 @@ print(data['class'].unique())
 
 # remove redundant/irrelevant attributes
 
-data = data.drop(['Curricular units 1st sem (credited)', 'Curricular units 1st sem (without evaluations)', 
-            'Curricular units 2nd sem (credited)', 'Curricular units 2nd sem (without evaluations)'], axis=1)
-print(data.describe())
+# data = data.drop(['Curricular units 1st sem (credited)', 'Curricular units 1st sem (without evaluations)', 
+#             'Curricular units 2nd sem (credited)', 'Curricular units 2nd sem (without evaluations)'], axis=1)
+# print(data.describe())
 
 # remove outliers
 # correct measures / adjust scale
@@ -41,13 +49,10 @@ print(data.describe())
 # ... etc ...
 
 
-
-
-
 # -------------
 # SAVE NEW DATA
 # -------------
 
-data.to_csv('data/data-clean.csv', index=False)
+# data.to_csv('data/data-clean.csv', index=False)
 # data_clean = pd.read_csv('data/data-clean.csv')
 # sb.pairplot(data_clean, hue='class')
